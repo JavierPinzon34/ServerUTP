2b9ab0031447b4a981cec6e0495d36fc0c540840
Javier <jspinzonr@misena.edu.co>
2021-10-09 12:08:38 -0500

    change header cords

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json())
 
 //CORDS
 app.use((req, res, next)=>{
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("*", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, ContentType, Accept");
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
