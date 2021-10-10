d847df8c53c744c49ab50dc2de0556ee9a467d46
Javier <jspinzonr@misena.edu.co>
2021-10-09 20:40:12 -0500

    crud login and crypt password

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 const cors = require("cors");
-
 var app = express()
 
 app.use(cors());
@@ -14,6 +13,7 @@ var user_routes = require('./routes/user')
 var service_routes = require('./routes/service')
 var article_routes = require('./routes/article')
 var category_routes = require('./routes/category')
+var auth_routes = require('./routes/auth');
 
 //midlewares
 app.use(bodyParser.urlencoded({extended:false}))
@@ -34,6 +34,7 @@ app.use('/api/', user_routes)
 app.use('/api/', service_routes)
 app.use('/api/', article_routes)
 app.use('/api/', category_routes)
+app.use('/api/auth/', auth_routes);
 
 //exportar
 module.exports = app
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,16 @@
 'use strict'
 
+var passport = require("passport");
+
 var express = require('express')
 var UserController = require('../controllers/UserController')
 
 var router = express.Router()
 
-router.get('/user', UserController.index)
-router.get('/user/:id?', UserController.show)
+router.get('/user', passport.authenticate("jwt", { session: false }), UserController.index)
+router.get('/user/:id?', passport.authenticate("jwt", { session: false }), UserController.show)
 router.post('/user', UserController.store)
-router.put('/user/:id?', UserController.update)
-router.delete('/user/:id?', UserController.delete)
+router.put('/user/:id?', passport.authenticate("jwt", { session: false }), UserController.update)
+router.delete('/user/:id?', passport.authenticate("jwt", { session: false }), UserController.delete)
 
 module.exports = router
